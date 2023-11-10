ab7e7829bae80b50a8c22ee771f7e0d41d1adeb5
John Chacón <enlinea777@gmail.com>
2023-11-10 20:18:19 -0300

    carga inicial

---
+++ README.md
@@ -0,0 +1,319 @@
+# JQuery-Ajax-Uploader
+
+This lightweight JQuery plugin enables basic Ajax file uploading features (select, drag & drop) on the frontend side. It is supposed to work well with any Laravel backend.  
+
+## 1. Requirements
+
+- This plugin does not support the old versions of Internet Explorer.  
+- This plugin requires Bootstrap 4.x  
+
+You should make sure that the page html layout has the following ```<meta>``` tag:  
+```html
+<meta name="csrf-token" content="{{ csrf_token() }}">
+```
+
+Otherwise, the request may be blocked by Laravel by default. See more details at https://laravel.com/docs/5.8/csrf#csrf-x-csrf-token.  
+
+The plugin automatically checks the existence of this ```<meta>``` tag, get its content and associate ```X-CSRF-TOKEN``` header with the uploading request.  
+ 
+Alternatively, ***if and only if*** this ```<meta>``` tag does not exist (maybe you do not want to use, or for somewhat reasons), then you can include ```X-CSRF-TOKEN``` request header before sending files to server via ```beforeSend(xhr)``` hook as follows:  
+
+```javascript
+$(document).ready(function () {
+    $('#image-upload').enlinea777_ajaxuploader({
+        ...
+        beforeSend: (xhr) => {
+            xhr.setRequestHeader('X-CSRF-TOKEN', {!! json_encode(csrf_token()) !!});
+        },
+        ...
+    });
+});
+```
+
+## 2. Installation
+
+    npm install @bkstar18/jquery-ajax-uploader
+
+## 3. Usage
+
+**In HTML section**:  
+Insert a normal file input tag as follows:  
+```html
+<!-- use 'multiple' if you want to allow multiple files selection -->
+<input type="file" class="form-control" name="image" id="image-upload" multiple>
+```
+
+**In Javascript section**:  
+```javascript
+// JQuery must be loaded before this section
+<script src="/path/to/enlinea777-ajax-uploader.min.js"></script> // This minified JS file can be found at node_modules/@bkstar18/jquery-ajax-uploader/dist/ path
+<script type="text/javascript">
+$(document).ready(function () {
+    $('#image-upload').enlinea777_ajaxuploader(settings);
+});
+</script>
+```
+
+Passing ```settings``` to ```enlinea777_ajaxuploader()``` is optional. If you do not pass any argument, then the plugin will use its defaults as follows:  
+
+```javascript
+defaults = {
+    size: 5242880, // In bytes <=> 5MB
+    uploadUrl: '/upload', // Backend URL to upload file
+    batchSize: 5, // Maximum number of files that can be uploaded in parallel
+    allowedExtensions: ['png', 'jpg', 'jpeg', 'mp4', 'doc', 'docx', 'ppt', 'pptx', 'xls', 'xlsx', 'txt', 'pdf'], // Accepted file extensions
+    progressBarColor: 'bg-primary', // set the color of the progress bar, see https://getbootstrap.com/docs/4.3/components/progress/
+}
+```
+
+All these default settings can be overwritten by the ```settings``` argument that you pass to ```enlinea777_ajaxuploader()```.  
+
+**Important Note**:  
+The plugin expects to receive JSON response from the backend with ```success``` key in case of success or ```error``` key in case of error.  
+
+Sample structure of JSON responses are as follows:  
+***a) Success response***  
+```javascript
+{
+    "success": "Successfully uploaded", // mandatory key
+    ...  // other key that you may add
+}
+```
+
+***b) Error response***
+```javascript
+{
+    "error": "Upload failed", // mandatory key
+    ...  // other key that you may add
+}
+```
+
+**Advanced use**:  
+The plugin will automatically wrap the selected ```<input type="file" />```  element in an outer element ```<div class="enlinea777-ajaxuploader-container-image-upload" id="enlinea777-ajaxuploader-container-image-upload">```  
+
+Where ```enlinea777-ajaxuploader``` is a prefix that you can change by ```prefix``` key in the ```settings``` argument.  
+
+You can also add more class to the wrapping element by ```outerClass``` key in the ```settings``` argument.  
+
+There are two callback hooks that you can use, that are, ```beforeSend(xhr)``` and ```onResponse(response)```:  
+
+- ```beforeSend(xhr)``` hook is called right before sending files to the server, and it is passed with the whole XmlHttpRequest object. You can use this hook to modify the behavior of XHR object before it start sending files to the server, for example to add more request header like ```xhr.setRequestHeader(header, value)```.  
+
+- ```onResponse(response)``` hook is called when the uploading process is completed (xhr.readystate = 4, no matter success or failure). It is passed with the whole server response.  
+
+**Example**:  
+```javascript
+$('#image-upload').enlinea777_ajaxuploader({
+    allowedExtensions: ['png','jpg','jpeg'],
+    batchSize: 5,
+    outerClass: 'col-md-12',
+    uploadUrl: '/api/upload',
+    beforeSend: (xhr) => {
+        xhr.setRequestHeader('X-AUTHOR', 'TUANHA');
+    },
+    onResponse: (response) => {
+        let res = JSON.parse(response)
+        if (res.data) {
+            $('#gallery').append(`<img id="${res.data.filename}" src="${res.data.url}" width="50px">`);
+        }
+    }
+});
+```
+
+## 4. Demo
+
+This section will demonstrate how to build a completed system (both backend & frontend) for handling Ajax file upload.  
+
+**Backend**:
+- Built from a new & clean Laravel application with the use of ***enlinea777/laravel-uploader*** package (written by myself)  
+(See its full documentation at https://github.com/enlinea777/laravel-uploader)     
+
+**Frontend**:
+- Built with this JQuery plugin ***@bkstar18/jquery-ajax-uploader***, of course :-)  
+
+### 4.1 Preparation
+
+**a) Create a new Laravel application**   
+```laravel new testproject```  
+
+**b) Create a symlink between ```public/storage``` and ```storage/app/public```**  
+```cd testproject & php artisan storage:link```  
+
+**c) Create a photos migration**  
+```php artisan make:migration create_photos_table --table```
+
+Place the following lines in the ```up()``` function:  
+```php
+<?php
+...
+Schema::create('photos', function (Blueprint $table) {
+    $table->bigIncrements('id');
+    $table->string('filename');
+    $table->string('path');
+    $table->string('url');
+    $table->string('disk');
+    $table->timestamps();
+});
+...
+```  
+
+**d) Create ```App\Photo``` model**  
+```php artisan make:model Photo```  
+
+Place the following lines:  
+```php
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+    protected $fillable = [
+        'filename', 'url', 'path', 'disk'
+    ];
+}
+```
+
+**e) Run migration**    
+```php artisan migration```  
+
+**Note**: Of course, you must configure the database connection in ```.env```
+
+**f) Create upload routes**  
+In ```routes/web.php```, place the following routes:  
+
+```php
+Route::get('/upload', 'UploadController@showUploadForm');
+Route::post('/api/upload', 'UploadController@upload');
+```
+
+**g) Create controller**  
+```php artisan make:controller UploadController```  
+
+**h) Scaffolding your application**    
+```php artisan make:auth```
+
+The purpose of scaffolding is just to quickly generate the master layout and other basic views for your application. 
+
+**i) Install npm packages**  
+```npm install``` 
+
+### 4.2 Integrate enlinea777/laravel-uploader package to backend
+
+**a) Install enlinea777/laravel-uploader package**  
+```composer require enlinea777/laravel-uploader```  
+
+**b) Install @bkstar18/jquery-ajax-uploader plugin**  
+```npm install --save-dev @bkstar18/jquery-ajax-uploader```
+
+**c) Bundle the plugin into the main app.js**  
+- Place the following line in the ```resources/js/bootstrap.js```
+```javascript
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+    require('@bkstar18/jquery-ajax-uploader'); // Add this line
+} catch (e) {}
+```
+
+- Compile frontend assets  
+```npm run dev```  or ```npm run production```  
+
+**d) In ```UploadController```**  
+```php
+<?php
+namespace App\Http\Controllers;
+
+use App\Photo;
+use Illuminate\Http\Request;
+use Bkstar123\LaravelUploader\Contracts\FileUpload;
+
+class UploadController extends Controller
+{
+    public function showUploadForm()
+    {
+        return view('upload');
+    }
+
+    public function upload(Request $request, FileUpload $fileupload)
+    {
+        $data = $fileupload->handle($request, 'image', ['allowedExtensions' => ['jpg', 'png', 'jpeg']]);
+        if (!$data) {
+            return response()->json(['error' => $fileupload->uploadError], 422);
+        }
+
+        Photo::create($data);
+        return response()->json(['success' => "{$data['filename']} has been successfully uploaded", 'data' => $data], 200);
+    }
+}
+```
+
+**e) In ```resources/views/upload.blade.php```**  
+
+Place the following code:  
+```html
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Upload</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <div class="form-group">
+                        <label for="image-upload">Upload Images</label>
+                        <input type="file" class="form-control" name="image" id="image-upload" multiple>
+                        <div class="gallery" id="gallery"></div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('js')
+<script type="text/javascript">
+$(document).ready(function () {
+    $('#image-upload').enlinea777_ajaxuploader({
+        allowedExtensions: ['png','jpg','jpeg'],
+        batchSize: 5,
+        outerClass: 'col-md-12',
+        uploadUrl: '/api/upload',
+        beforeSend: (xhr) => {
+            xhr.setRequestHeader('X-AUTHOR', 'TUANHA');
+        },
+        onResponse: (response) => {
+            let res = JSON.parse(response)
+            if (res.data) {
+                $('#gallery').append(`<img id="${res.data.filename}" src="${res.data.url}" width="50px">`);
+            }
+        }
+    });
+});
+</script>
+@endpush
+```
+
+**Note:** In ```resources/views/layouts/app.blade.php```, you may need to remove ```defer``` from  
+```html
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script> <!-- remmove this 'defer' -->
+```  
+
+and add ```@stack('js')``` before the closing body tag ```</body>```.  
+
+***--DONE--***
---
+++ src/index.js
@@ -0,0 +1,259 @@
+/**
+ * jQuery enlinea777-ajaxuploader plugin
+ * author: John
+ * last-mod: 10-11-2023
+ * Licensed under the GPLv3 license
+ */
+;
+(function($, window, document, undefined) {
+    "use strict";
+    let pluginName = 'enlinea777_ajaxuploader';
+    $.fn[pluginName] = function(options) {
+        // Plugin settings
+        let settings = $.extend({}, $.fn[pluginName].defaults, options);
+
+        // Upload Input DOM Id & selector& name
+        let uploadInputId = $(this).attr('id');
+        let uploadInputSelector = `#${uploadInputId}`;
+        let uploadInputName = $(this).attr('name');
+
+        // DOM Id & selector for the root container of upload section
+        let uploadContainerDivId = `${settings.prefix}-container-${uploadInputId}`;
+        let uploadContainerDivSelector = `#${uploadContainerDivId}`;
+
+        // DOM Id & selector for the container of error alert
+        let uploadErrorContainerDivId = `${settings.prefix}-error-for-${uploadInputId}`;
+        let uploadErrorContainerDivSelector = `#${uploadErrorContainerDivId}`;
+
+        // DOM Id & selector for the container of success alert
+        let uploadSuccessContainerDivId = `${settings.prefix}-success-for-${uploadInputId}`;
+        let uploadSuccessContainerDivSelector = `#${uploadSuccessContainerDivId}`;
+
+        // DOM Id & selector for the container of progress bars
+        let progressContainerDivId = `${settings.prefix}-progress-${uploadInputId}`;
+        let progressContainerDivSelector = `#${progressContainerDivId}`;
+
+        // Wrap file input in the upload container
+        createUploadContainerDiv(uploadInputId);
+
+        // Validate file extension, size
+        function validateFile(file, uploadInputId) {
+            let fileExtension = file.name.split('.').pop().toLowerCase();
+            if (settings.allowedExtensions.indexOf(fileExtension) < 0) {
+                $(uploadErrorContainerDivSelector).append(`<li>${fileExtension} extension is not allowed</li>`);
+                $(uploadErrorContainerDivSelector).show();
+                return false;
+            }
+
+            if (file.size > settings.size) {
+                $(uploadErrorContainerDivSelector).append(`<li>${file.name} (${file.size} bytes) exceeds upload limit</li>`);
+                $(uploadErrorContainerDivSelector).show();
+                return false;
+            }
+            return true;
+        }
+
+        // Validate batch size <-> max number of files in parallel upload
+        function validateBatch(files, uploadInputId) {
+            if (files.length > settings.batchSize) {
+                $(uploadErrorContainerDivSelector).append(`<li>The upload exceeds the limit of ${settings.batchSize} files</li>`);
+                $(uploadErrorContainerDivSelector).show();
+                return false;
+            }
+            return true;
+        }
+
+        // Check if a DOM element exists
+        function elementExist(selector) {
+            return $(selector).length > 0;
+        }
+
+        // add upload alert
+        function addUploadAlert(containerSelector, message) {
+            if (message && typeof message === 'string') {
+                $(containerSelector).append(`<li>${message}</li>`);
+                $(containerSelector).show();
+            }
+        }
+
+        // Remove the container of all progress bars, remove error/success alert containers, clear file input
+        function resetUploadStatus(uploadInputId) {
+            $(progressContainerDivSelector).remove();
+            $(uploadErrorContainerDivSelector).remove();
+            $(uploadSuccessContainerDivSelector).remove();
+            $(uploadInputSelector).val('');
+        }
+
+        // Create error/success alert container
+        function showUploadStatus(uploadInputId) {
+            createUploadErrorContainerDiv(uploadInputId);
+            createUploadSuccessContainerDiv(uploadInputId);
+        }
+
+        // Create an outer container div for uploading section
+        function createUploadContainerDiv(uploadInputId) {
+            if (!elementExist(uploadContainerDivSelector)) {
+                $(uploadInputSelector).wrap(`<div class="${uploadContainerDivId}" id="${uploadContainerDivId}"></div>`);
+                $(uploadContainerDivSelector).addClass(settings.outerClass);
+            }
+        }
+
+        // Create a container for error alert
+        function createUploadErrorContainerDiv(uploadInputId) {
+            if (!elementExist(uploadErrorContainerDivSelector)) {
+                let html = '<div class="alert alert-danger"' +
+                    ` id="${uploadErrorContainerDivId}"` +
+                    ' style="display:none" role="alert"></div>';
+                $(uploadContainerDivSelector).append(html);
+            }
+        }
+
+        // Create a container for success alert
+        function createUploadSuccessContainerDiv(uploadInputId) {
+            if (!elementExist(uploadSuccessContainerDivSelector)) {
+                let html = '<div class="alert alert-success"' +
+                    ` id="${uploadSuccessContainerDivId}"` +
+                    ' style="display:none" role="alert"></div>';
+                $(uploadContainerDivSelector).append(html);
+            }
+        }
+
+        // Create a progress container for the selected file input which holds a progress bar for each upload file
+        function createUploadProgressContainerDiv(uploadInputId, fileIndex, file) {
+            if (!elementExist(progressContainerDivSelector)) {
+                $(uploadContainerDivSelector).prepend(`<div id="${progressContainerDivId}"></div>`); // progress container
+            }
+
+            // sub-container for each upload file
+            let uploadProgressBarDivId = `${settings.prefix}-progress-bar-${uploadInputId}-${fileIndex}`;
+            if (!elementExist(`#${uploadProgressBarDivId}`)) {
+                let label = `<label>${file.name}</label>`;
+                let progressBarHTML = '<div class="progress" style="height:1.2rem">' +
+                    `<div class="progress-bar ${settings.progressBarColor} progress-bar-striped"` +
+                    ` id="${uploadProgressBarDivId}"` +
+                    ' role="progressbar" style="width: 0%" aria-valuenow="0"' +
+                    ' aria-valuemin="0" aria-valuemax="100"></div></div>';
+                $(progressContainerDivSelector).append(label + progressBarHTML);
+            }
+        }
+
+        // Send file to server
+        function sendFile(fileIndex, file, uploadInputId, uploadInputName) {
+            if (!validateFile(file, uploadInputId)) {
+                return;
+            }
+            createUploadProgressContainerDiv(uploadInputId, fileIndex, file);
+
+            let fd = new FormData();
+            fd.append(uploadInputName, file);
+
+            let xhr = new XMLHttpRequest();
+            let uploadProgressBarDivSelector = `#${settings.prefix}-progress-bar-${uploadInputId}-${fileIndex}`;
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        addUploadAlert(uploadSuccessContainerDivSelector, JSON.parse(xhr.responseText).success);
+                    } else {
+                        addUploadAlert(uploadErrorContainerDivSelector, JSON.parse(xhr.responseText).error);
+                        $(uploadProgressBarDivSelector).removeClass(settings.progressBarColor);
+                        $(uploadProgressBarDivSelector).addClass('bg-danger');
+                    }
+                    settings.onResponse(xhr.response);
+                }
+            };
+            xhr.upload.onprogress = function(event) {
+                if (event.lengthComputable) {
+                    let percentComplete = event.loaded / event.total;
+                    percentComplete = parseInt(percentComplete * 100);
+                    $(uploadProgressBarDivSelector).css('width', `${percentComplete}%`);
+                    $(uploadProgressBarDivSelector).text(`${percentComplete}%`);
+                }
+            };
+
+            xhr.open('POST', settings.uploadUrl, true);
+
+            let token = document.head.querySelector('meta[name="csrf-token"]');
+            if (token) {
+                xhr.setRequestHeader('X-CSRF-TOKEN', token.content);
+            } else {
+                console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+            }
+
+            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            settings.beforeSend(xhr);
+            xhr.send(fd);
+        }
+
+        // Upload by selecting files
+        $(this).click(function() {
+            resetUploadStatus(uploadInputId);
+        });
+
+        $(this).change(function(e) {
+            e.preventDefault();
+            showUploadStatus(uploadInputId);
+            let files = $(this)[0].files;
+            if (!validateBatch(files, uploadInputId)) {
+                return;
+            }
+            for (let i = 0; i < files.length; i++) {
+                sendFile(i, files[i], uploadInputId, uploadInputName);
+            }
+        });
+
+        // Upload by dragging & dropping files
+        let dragDropEvents = ['dragenter', 'dragover', 'drop', 'dragleave'];
+        dragDropEvents.forEach(eventName => {
+            $(uploadContainerDivSelector)[0].addEventListener(eventName, (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+            }, false)
+        });
+
+        function highlightDragDropArea() {
+            $(uploadContainerDivSelector).css('border', '1px solid black');
+            $(uploadContainerDivSelector).css('padding', '2% 2% 20% 2%');
+            $(uploadContainerDivSelector).css('background-color', '#ebf0f2');
+        }
+
+        function removeHighlightDragDropArea() {
+            $(uploadContainerDivSelector).removeAttr('style');
+        }
+
+        function handleDragEnter(e) {
+            resetUploadStatus(uploadInputId);
+            highlightDragDropArea();
+        }
+
+        function handleDragLeave(e) {
+            removeHighlightDragDropArea();
+        }
+
+        function handleDrop(e) {
+            showUploadStatus(uploadInputId);
+            removeHighlightDragDropArea();
+            let dt = e.dataTransfer;
+            let files = dt.files;
+            for (let i = 0; i < files.length; i++) {
+                sendFile(i, files[i], uploadInputId, uploadInputName);
+            }
+        }
+
+        $(uploadContainerDivSelector)[0].addEventListener('dragenter', handleDragEnter, false);
+        $(uploadContainerDivSelector)[0].addEventListener('dragleave', handleDragLeave, false);
+        $(uploadContainerDivSelector)[0].addEventListener('drop', handleDrop, false);
+
+        return this; //for chainablity
+    };
+    $.fn[pluginName].defaults = {
+        size: 500242880, //500MB
+        uploadUrl: '/upload',
+        batchSize: 5,
+        outerClass: '',
+        progressBarColor: 'bg-primary',
+        prefix: 'enlinea777-ajaxuploader',
+        allowedExtensions: ['png', 'jpg', 'jpeg', 'mp4', 'doc', 'docx', 'ppt', 'pptx', 'xls', 'xlsx', 'txt', 'exe', 'pdf'],
+        beforeSend(xhr) {},
+        onResponse(response) {}
+    };
+})(jQuery, window, document);
